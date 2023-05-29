61018d4bf86e4ae1b7720370ab8110b4dc911a3e
Eduardo Ureta <eduardo.uretaca@gmail.com>
2023-05-29 13:11:56 -0400

    Final JS

--- main.js
+++ main.js
@@ -1,117 +1,211 @@
-// /// Class Productos
+///// obtener contenido del catalogo
+const contenidoCatalogo = document.getElementById("contenidoCatalogo");
 
-// class Productos{
-//     constructor(info){
-//         this.id = info.id
-//         this.nombre = info.nombre,
-//         this.precio = info.precio,
-//         this.img = info.img
-//         this.stock = 1
-//     };
-// };
-
-// //// Instanciar productos
-
-// const pisco = new Productos(1, "Pisco", 6990, "../recursos/productos/pisco_low.png");
-// const Ron = new Productos(2, "Ron", 5990, "../recursos/productos/ron_low.png");
-// const vodka = new Productos(3, "Vodka", 4990, "../recursos/productos/vodka_low.png");
-// const tequila = new Productos(4, "Tequila", 7990, "../recursos/productos/tequila_low.png");
-// const cerveza = new Productos(5, "Cervezas", 3990, "../recursos/productos/cervezas_low.png");
-// const whisky = new Productos(6, "Whisky", 9990, "../recursos/productos/whisky_low.png");
-// const jugo = new Productos(7, "Jugos", 1990, "../recursos/productos/jugos_low.png");
-// const bebida = new Productos(8, "Bebidas", 1990, "../recursos/productos/bebidas_low.png");
-
-// //// Lista
-
-// const productos = [pisco, Ron, vodka, tequila, cerveza, whisky, jugo, bebida];
-
-// ///// Carrito
-
-// let carrito = [];
-
-// //cargar carrito desde el localstorage
-
-// carrito = (localStorage.getItem('carrito')) ? JSON.parse(localStorage.getItem('carrito')) : [];
-
-// // Modificar DOM para mostrar los productos
-// const contenedorProductos = document.getElementById("contenedorProductos");
-
-
-
-
-
-////////////////
+//// obtener contenido del carrito
 const contenidoCarrito = document.getElementById("contenidoCarrito");
 
+/// boton ver carrito
+const verCarrito = document.getElementById("verCarrito");
+
+//// Vaciar carrito
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+
+// const modalContainer = document.getElementById("modal-container")
+
 const productos = [
-    {
-        id: 1,
-        nombre: "Pisco",
-        precio: 6990,
-        img: "../recursos/productos/pisco_low.png",
-    },
-    {
-        id: 2,
-        nombre: "Ron",
-        precio: 5990,
-        img: "../recursos/productos/ron_low.png",
-    },
-    {
-        id: 3,
-        nombre: "Vodka",
-        precio: 4990,
-        img: "../recursos/productos/vodka_low.png",
-    },
-    {
-        id: 4,
-        nombre: "Tequila",
-        precio: 7990,
-        img: "../recursos/productos/tequila_low.png",
-    },
-    {
-        id: 5,
-        nombre: "Cerveza",
-        precio: 3990,
-        img: "../recursos/productos/cervezas_low.png",
-    },
-    {
-        id: 6,
-        nombre: "Whisky",
-        precio: 9990,
-        img: "../recursos/productos/whisky_low.png",
-    },
-    {
-        id: 7,
-        nombre: "Jugos",
-        precio: 1990,
-        img: "../recursos/productos/jugos_low.png",
-    },
-    {
-        id: 8,
-        nombre: "Bebidas",
-        precio: 1990,
-        img: "../recursos/productos/bebidas_low.png",
-    },
+    {id: 1, nombre: "Pisco", precio: 6990, img: "../recursos/productos/pisco_low.png", cantidad: 1},
+    {id: 2, nombre: "Ron", precio: 5990, img: "../recursos/productos/ron_low.png", cantidad: 1},
+    {id: 3, nombre: "Vodka", precio: 4990, img: "../recursos/productos/vodka_low.png", cantidad: 1},
+    {id: 4, nombre: "Tequila",precio: 7990, img: "../recursos/productos/tequila_low.png", cantidad: 1},
+    {id: 5, nombre: "Cerveza", precio: 3990, img: "../recursos/productos/cervezas_low.png", cantidad: 1},
+    {id: 6, nombre: "Whisky", precio: 9990, img: "../recursos/productos/whisky_low.png", cantidad: 1},
+    {id: 7, nombre: "Jugos", precio: 1990, img: "../recursos/productos/jugos_low.png", cantidad: 1},
+    {id: 8,nombre: "Bebidas", precio: 1990,img: "../recursos/productos/bebidas_low.png", cantidad: 1},
 ];
 
 //// Carrito 
 
 let carrito = [];
 
+//// Cargar carrito desde el localstorage
+
+carrito = (localStorage.getItem('carrito')) ? JSON.parse(localStorage.getItem('carrito')) : [];
+
 /// Recorrer el array de productos y crear el codigo HTML con cada producto
 
 productos.forEach((producto) => {
     let contenedor = document.createElement("div"); /// Creo el div
-    contenedor.className = "border col-sm-6 col-md-4 col-lg-3";
+    contenedor.className = "col-sm-6 col-md-4 col-lg-3";
     contenedor.innerHTML = `  
         <h1 class="text-center text-warning">${producto.nombre}</h1>
         <img class="img-fluid imgAnimado bg-light" src="${producto.img}"><img>
         <p class="text-center h3 text-warning">$${producto.precio}</p>
     ` ///Creo el HTML de cada producto
-    contenidoCarrito.append(contenedor); /// en el div del HTML, pego lo creado recién
+    contenidoCatalogo.append(contenedor); /// en el div del HTML, pego lo creado recién
 
     let botonComprar = document.createElement("button"); /// Creo un boton html
     botonComprar.innerHTML = "Añadir al carrito"; /// con un texto
+    botonComprar.className = "bg-secondary border text-light buttonAnadir"
     contenedor.append(botonComprar); /// en el div del html, pego lo recien creado
+
+
+/// Escuchar evento // Cargar carrito con selección de productos
+
+//// Si el producto ya está en carrito, agrega 1 cantidad, si no, crealo
+    botonComprar.addEventListener("click", () =>{
+        let productoEnCarrito = carrito.find(item => item.id === producto.id);
+        if (productoEnCarrito) {
+          productoEnCarrito.cantidad++;
+        }else{
+        carrito.push({
+            id: producto.id,
+            img: producto.img,
+            nombre: producto.nombre,
+            precio: producto.precio,
+            cantidad: 1,
+        })};
+        Toastify({
+            text: `Has agregado ${producto.nombre}`,
+            duration: 3000,
+            style:{
+                background: "linear-gradient(to right, #00b09b, #96c92d)",
+            },
+        }).showToast();
+    });
+});
+
+//// Ver productos agregados en el carrito ////
+
+verCarrito.addEventListener("click", () =>{
+    mostrarCarrito();
+});
+
+const mostrarCarrito = () => {
+    contenidoCarrito.innerHTML = "";  /// Para que al apretar, se agregue solo lo nuevo
+    carrito.forEach((producto) => {
+        let card = document.createElement("div");
+        card.classList.add("col-xs-4", "col-sm-4", "col-md-4", "col-lg-3");
+        card.innerHTML = `
+        <div class="card">
+            <img class="img-fluid bg-light card-img-top" width="250px" src="${producto.img}"></img>
+            <h3 class="text-center text-dark">${producto.nombre}</h3>
+            <p class="h3 text-center text-dark">${producto.precio}</p>
+            <h4 class="text-center text-dark">Cantidad: ${producto.cantidad}</h4>
+        </div>
+        <div> 
+            <button class="btn btn-light" id="restar${producto.id}">-</button>
+            <button class="btn btn-light" id="agregar${producto.id}">+</button>
+            <button class="border btn colorBoton text-light bg-danger" id="eliminar${producto.id}">Eliminar</button>
+        </div>
+        `;
+        contenidoCarrito.appendChild(card);
+
+        //// Eliminar productos
+        const botonEliminar = document.getElementById(`eliminar${producto.id}`);
+        botonEliminar.addEventListener("click", () => {
+          eliminar(producto.id);
+        });
+        
+        //Restar productos
+        const botonRestar = document.getElementById(`restar${producto.id}`);
+        botonRestar.addEventListener("click", () => {
+          restar(producto.id);
+        });
+
+        //// Agregar productos
+        const botonSumar = document.getElementById(`agregar${producto.id}`);
+        botonSumar.addEventListener("click", () => {
+          agregar(producto.id);
+        });
+    
+    });
+    ///// Alerta de carrito actualizado o vacio
+    if(carrito.length >= 1){
+        Swal.fire(
+            "¡Excelente!",
+            "Ahora verás tu carrito de compras actualizado",
+            "success",
+        );
+    }else{
+        Swal.fire(
+            "¡Aún no has agregado productos al carrito!",
+            "Selecciona tu producto preferido",
+            "warning",
+        );
+    };
+    //// Mostrar total a pagar
+    totalPagar();
+    //localstorage de carrito total
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+
+//// Función agregar productos
+const agregar = (id) => {
+    const producto = carrito.find(producto => producto.id === id);
+        if(producto.cantidad >= 1){
+        producto.cantidad +=1;
+        mostrarCarrito();
+    //localstorage
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }};
+  
+  //// Función restar productos
+  const restar = (id) => {
+    const producto = carrito.find(producto => producto.id === id);
+    if (producto.cantidad > 1) {
+      producto.cantidad -= 1;
+      mostrarCarrito();
+      //localstorage
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+    }else{
+      eliminar(id);
+    };
+  };
+  
+//// Función eliminar productos
+  const eliminar = (id) => {
+    carrito = carrito.filter((producto) => producto.id !== id);
+    mostrarCarrito();
+  };
+
+
+//// Función total a pagar
+const totalPagar = () => {
+    const total = carrito.reduce((acum, item) => acum + (item.precio * item.cantidad), 0);
+    const totalCompra = document.createElement("div");
+    totalCompra.className = "total-content";
+    totalCompra.innerHTML = `<h2 class="text-center text-warning">Total a pagar: </h2> <h1 class="text-light"> ${total} CPL </h1>`
+    contenidoCarrito.append(totalCompra);
+};
+    
+//// Boton vaciar carrito
+
+vaciarCarrito.addEventListener("click", () =>{
+    carrito = [];
+    contenidoCarrito.innerHTML = "";
+    Swal.fire(
+        "¡Has eliminado tu carrito!",
+        "Selecciona nuevamente tu producto preferido",
+        "info",
+    );
+    //localstorage
+    localStorage.clear();
+});
+
+//// Boton finalizar compra
+
+finalizarCompra.addEventListener("click", () =>{
+    if(carrito.length >=1){
+    Swal.fire(
+        "¡Has Finalizado tu compra!",
+        "Te rediccionaremos a la pasarela de pagos",
+        "success",
+    )}else{
+        Swal.fire(
+            "¡No has agregado productos a tu carrito!",
+            "Favor selecciona tu producto favorito",
+            "warning",
+    )};
 });
 
